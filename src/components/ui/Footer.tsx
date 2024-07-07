c3955780c9e399516e5d9812510e47650a04b5bf
briskaputraa <briskaananda103@gmail.com>
2024-07-07 16:48:17 +0700

    Instagram Credit

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <div className="w-full p-4 rounded-md">
-      <h1 className="text-lg text-slate-900 font-medium text-end opacity-80 hover:opacity-100"> <a href="www.instagram.com/briskaanandaa">Crafted by Briska</a></h1>
+      <h1 className="text-lg text-slate-900 font-medium text-end opacity-80 hover:opacity-100"> <a href="https://www.instagram.com/briskaanandaa">Crafted by Briska</a></h1>
     </div>
   )
 }
